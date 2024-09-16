3bdd01ea3df3babf764dd6ccf9fe21b2ba0e8f5f
Muhammad Ishaq Khan <109451565+imishaqkhan@users.noreply.github.com>
2024-09-16 16:54:12 +0500

    Update script.js

--- practice test/javascript/script.js
+++ practice test/javascript/script.js
@@ -12,29 +12,29 @@ const groceries = (items) =>{
   return data.item;
  })
 
- if(filteredItemList.length ==0) return "";
- if(filteredItemList.length == 1) return filteredItemList[0];
+//  if(filteredItemList.length ==0) return "";
+//  if(filteredItemList.length == 1) return filteredItemList[0];
 
-let lastIndexValue = filteredItemList.pop();
+// let lastIndexValue = filteredItemList.pop();
 
-return `${filteredItemList.join(', ')}  and ${lastIndexValue}`
+// return `${filteredItemList.join(', ')}  and ${lastIndexValue}`
 
-//  let lastValue = filteredItemList[filteredItemList.length - 1];
-//  let result = filteredItemList.join(', ');
+ let lastValue = filteredItemList[filteredItemList.length - 1];
+ let result = filteredItemList.join(', ');
 
-//  return result.replace(', '+lastValue, ' and '+lastValue)
+ return result.replace(', '+lastValue, ' and '+lastValue)
 
-// let output = ""
-// let lastvalue = filteredItemList.pop();
-//  for(let i=0; i < filteredItemList.length; i++){
-//   if(output !=""){
-//     output = output +', '+ filteredItemList[i] 
-//   }
-//   else{
-//     output = filteredItemList[i] 
-//   }
-//  }
-// console.log(output + ' and '+ lastvalue)
+let output = ""
+let lastvalue = filteredItemList.pop();
+ for(let i=0; i < filteredItemList.length; i++){
+  if(output !=""){
+    output = output +', '+ filteredItemList[i] 
+  }
+  else{
+    output = filteredItemList[i] 
+  }
+ }
+console.log(output + ' and '+ lastvalue)
   
 }
 
